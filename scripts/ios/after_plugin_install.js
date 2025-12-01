2a2b05d75cf7db08259181f57da5ae286c37e975
wwannous <wwannous@users.noreply.github.com>
2025-11-30 21:50:19 -0600

    f

--- scripts/ios/after_plugin_install.js
+++ scripts/ios/after_plugin_install.js
@@ -1,10 +1,5 @@
-var helper = require("./helper");
-
+// Disabled for Appflow compatibility.
+// The original script attempted to modify the Xcode project before it exists.
 module.exports = function(context) {
-
-    // Add a build phase which runs a shell script that executes the Crashlytics
-    // run command line tool which uploads the debug symbols at build time.
-    var xcodeProjectPath = helper.getXcodeProjectPath();
-    helper.removeShellScriptBuildPhase(context, xcodeProjectPath);
-    helper.addShellScriptBuildPhase(context, xcodeProjectPath);
+    return;
 };
